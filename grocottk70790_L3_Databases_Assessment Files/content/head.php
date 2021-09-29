d393a352d7f0bf1b862fd7b67a9a1b39eb08ffca
Kahlil <51727305+grocottk@users.noreply.github.com>
2021-09-30 12:21:13 +1300

    Build after some colours and some content changed

--- grocottk70790_L3_Databases_Assessment Files/content/head.php
+++ grocottk70790_L3_Databases_Assessment Files/content/head.php
@@ -5,13 +5,13 @@
     <meta name="author" content="Kahlil">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    <title>Quick Quotes</title>
+    <title>Premium Poems</title>
     
     <!-- Edit the link below / replace with your chosen google font -->
     <link href="https://fonts.googleapis.com/css?family=Lato%7cUbuntu" rel="stylesheet"> 
     
     <!-- Edit the name of your style sheet - 'foo' is not a valid name!! -->
-    <link rel="stylesheet" href="theme/quick_quotes.css"> 
+    <link rel="stylesheet" href="theme/premium_poems.css"> 
     <link rel="stylesheet" href="theme/font-awesome.min.css">
     <link rel="stylesheet" href="theme/auto_complete.css">
     
