c3572eed1c075fd24bc6cc0f1b651faca7a89305
Kahlil <51727305+grocottk@users.noreply.github.com>
2021-09-29 13:29:08 +1300

    Build during initial website setup

---
+++ grocottk70790_L3_Databases_Assessment Files/config.php
@@ -0,0 +1,12 @@
+<?php
+
+// From "[...]config.php" in "[...]L3_Practice_Database"
+
+ini_set('display_errors', 'On');  // change to 'Off' when live
+
+define ("DB_NAME","grocottk70790_L3_Practice_Database"); 
+define ("DB_USERNAME","_grocottk70790");
+define ("DB_PASSWORD","v9Zg4mi6je7pQYPI"); 
+define ("DB_HOST","localhost");
+
+?>
---
+++ grocottk70790_L3_Databases_Assessment Files/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE HTML>
+
+<!-- From "[...]index.php" in "[...]L3_Site_Template"
+
+<?php
+
+session_start();
+include("config.php");
+include("functions.php");
+
+// Connect to database...
+$dbconnect=mysqli_connect(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_NAME);
+
+if(mysqli_connect_errno()) {
+    echo "Connection failed:".mysqli_connect_error();
+    exit;
+} // Checking for issues
+
+?>
+
+<html lang="en">
+
+<?php include("content/head.php"); ?>
+    
+<body>
+    
+    <div class="wrapper">
+
+            <?php include("content/heading_navigation.php") ?>
+        
+        <div class="box main">
+
+            <?php
+
+            if(!isset($_REQUEST['page'])) {
+                include("content/home.php");
+            } // This is the end that includes the home page
+
+            else {
+                // This prevents users from navigating through [a] file system
+                $page=preg_replace('/[^0-9a-zA-Z]-/','',$_REQUEST['page']);
+                include("content/$page.php");
+            } // This is the end that includes the user's requested content
+
+            ?>
+                    
+        </div>    <!-- / main -->
+        
+        <?php include("content/footer.php"); ?>
+    
+    </div>  <!-- / wrapper  -->
+    
+</body>
